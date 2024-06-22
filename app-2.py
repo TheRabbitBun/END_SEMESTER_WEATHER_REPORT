2657bf222e42daebb7e712672f60abe4325b9a1e
TheRabbitBun <95520492+TheRabbitBun@users.noreply.github.com>
2024-06-23 00:16:54 +0800

    7

--- app-2.py
+++ app-2.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-import json, time, requests
+import json, time, requests, math
 app = Flask(__name__)
-Test = "Tamshui"
+Test = "Ruisui" + ",TW"
 API_KEY = 'e8fef5845478923a029a170937d3e487'
 city_query = Test 
 geo_arr = f'http://api.openweathermap.org/geo/1.0/direct?q={city_query},TW&limit=1&appid={API_KEY}'
@@ -10,10 +10,10 @@ geo_loc_dt = geo_res.json()
 place = geo_loc_dt[0]
 lat = geo_loc_dt[0]["lat"]
 lon = geo_loc_dt[0]["lon"]
-print(place)
-def a(address):
-        
-        query = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}&lang=zh'
-        res = requests.get(query)
-        data = res.json()
+name_in_zh = place["local_names"]["zh"]
+query = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}&lang=zh'
+res = requests.get(query)
+data = res.json()
+print(data)
+
 #print(a(Test))
