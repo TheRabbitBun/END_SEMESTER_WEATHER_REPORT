ef619cb7e530e7cce7d7d728b5b49f13b6ef6732
TheRabbitBun <95520492+TheRabbitBun@users.noreply.github.com>
2024-06-22 20:56:42 +0800

    We don't need info abt eartthquake

--- app.py
+++ app.py
@@ -13,30 +13,30 @@ port = "5000"
 public_url = ngrok.connect(port).public_url
 print(f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\" ")
 
-def earth_quake():
-    result = []
-    code = '你的 token'
-    try:
-        # 小區域 https://opendata.cwa.gov.tw/dataset/earthquake/E-A0016-001
-        url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
-        req1 = requests.get(url)  # 爬取資料
-        data1 = req1.json()       # 轉換成 json
-        eq1 = data1['records']['Earthquake'][0]           # 取得第一筆地震資訊
-        t1 = data1['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
-        # 顯著有感 https://opendata.cwa.gov.tw/dataset/all/E-A0015-001
-        url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
-        req2 = requests.get(url2)  # 爬取資料
-        data2 = req2.json()        # 轉換成 json
-        eq2 = data2['records']['Earthquake'][0]           # 取得第一筆地震資訊
-        t2 = data2['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
+# def earth_quake():
+#     result = []
+#     code = '你的 token'
+#     try:
+#         # 小區域 https://opendata.cwa.gov.tw/dataset/earthquake/E-A0016-001
+#         url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
+#         req1 = requests.get(url)  # 爬取資料
+#         data1 = req1.json()       # 轉換成 json
+#         eq1 = data1['records']['Earthquake'][0]           # 取得第一筆地震資訊
+#         t1 = data1['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
+#         # 顯著有感 https://opendata.cwa.gov.tw/dataset/all/E-A0015-001
+#         url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
+#         req2 = requests.get(url2)  # 爬取資料
+#         data2 = req2.json()        # 轉換成 json
+#         eq2 = data2['records']['Earthquake'][0]           # 取得第一筆地震資訊
+#         t2 = data2['records']['Earthquake'][0]['EarthquakeInfo']['OriginTime']
         
-        result = [eq1['ReportContent'], eq1['ReportImageURI']] # 先使用小區域地震
-        if t2>t1:
-          result = [eq2['ReportContent'], eq2['ReportImageURI']] # 如果顯著有感地震時間較近，就用顯著有感地震
-    except Exception as e:
-        print(e)
-        result = ['抓取失敗...','']
-    return result
+#         result = [eq1['ReportContent'], eq1['ReportImageURI']] # 先使用小區域地震
+#         if t2>t1:
+#           result = [eq2['ReportContent'], eq2['ReportImageURI']] # 如果顯著有感地震時間較近，就用顯著有感地震
+#     except Exception as e:
+#         print(e)
+#         result = ['抓取失敗...','']
+#     return result
 
 def weather(address):
     try:
